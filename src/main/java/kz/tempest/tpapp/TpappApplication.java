b9f15d14d4ed50384b2aa4a35f578932bdfb04ba
biramzos <b.ramis.2002@gmail.com>
2024-06-27 21:55:33 +0500

    init project!

---
+++ src/main/java/kz/tempest/tpapp/TpappApplication.java
@@ -0,0 +1,13 @@
+package kz.tempest.tpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/tempest/tpapp/TpappApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.tempest.tpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
