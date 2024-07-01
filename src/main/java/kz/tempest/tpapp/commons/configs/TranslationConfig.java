d2275eb5b58ae3214f40b976129482f5fbbd0b78
biramzos <b.ramis.2002@gmail.com>
2024-07-02 01:03:03 +0500

    translation fixed!

--- src/main/java/kz/tempest/tpapp/commons/configs/TranslationConfig.java
+++ src/main/java/kz/tempest/tpapp/commons/configs/TranslationConfig.java
@@ -1,30 +1,20 @@
 package kz.tempest.tpapp.commons.configs;
 
-import kz.tempest.tpapp.commons.sources.IMessageSource;
 import kz.tempest.tpapp.commons.utils.TranslateUtil;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
-import java.io.IOException;
 
 @Configuration
 public class TranslationConfig {
-//    @Bean
-//    public ResourceBundleMessageSource messageSource() {
-//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-//        messageSource.setBasenames("messages");
-//        messageSource.setDefaultEncoding("UTF-8");
-//        messageSource.setUseCodeAsDefaultMessage(true);
-//        TranslateUtil.setMessageSource(messageSource);
-//        return messageSource;
-//    }
-
     @Bean
-    public MessageSource messageSource() {
-        IMessageSource iMessageSource = new IMessageSource();
-        TranslateUtil.setMessageSource(iMessageSource);
-        return iMessageSource;
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+        TranslateUtil.setMessageSource(messageSource);
+        return messageSource;
     }
 }
