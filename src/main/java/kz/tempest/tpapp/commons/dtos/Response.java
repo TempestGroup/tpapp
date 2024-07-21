3958891bcb7ef0f9c978920ae49d9bec0121b861
biramzos <b.ramis.2002@gmail.com>
2024-07-21 05:17:26 +0500

    fixed!

--- src/main/java/kz/tempest/tpapp/commons/dtos/Response.java
+++ src/main/java/kz/tempest/tpapp/commons/dtos/Response.java
@@ -1,12 +1,28 @@
 package kz.tempest.tpapp.commons.dtos;
 
-import java.util.IdentityHashMap;
+import lombok.SneakyThrows;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
-public class Response extends IdentityHashMap<String, Object> {
+public class Response extends HashMap<String, Object> {
 
     public static Response getResponse(String name, Object object) {
         return new Response() {{
             put(name, object);
         }};
+    }
+
+    @SneakyThrows
+    public static Response getResponse(Object object) {
+        Response response = new Response();
+        Class<?> objectClass = object.getClass();
+        while (objectClass != null) {
+            for (Field field : objectClass.getDeclaredFields()) {
+                field.setAccessible(true);
+                response.put(field.getName(), field.get(object));
+            }
+            objectClass = objectClass.getSuperclass();
+        }
+        return response;
     }
 }
